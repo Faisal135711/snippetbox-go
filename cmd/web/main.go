0ed5f9f061e1ea749e1844572b4700acbe303144
Faisal135711 <alpha316mnr@gmail.com>
2022-12-20 17:52:09 +0600

    add flash messages using session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"snippetbox.faisal135711.net/internal/models"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -20,6 +23,7 @@ type application struct {
 	snippets *models.SnippetModel
 	templateCache map[string]*template.Template
 	formDecoder *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -46,12 +50,17 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := application {
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &models.SnippetModel{DB: db},
 		templateCache: templateCache,
 		formDecoder: formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server {
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module snippetbox.faisal135711.net
 go 1.19
 
 require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20221206171621-0f0849773278 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
